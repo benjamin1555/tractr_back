8bcb3458a3f87ba1a11115043c17e1bafb30955a
Benjamin Teste <benjamin.teste@gmail.com>
2021-02-05 13:57:30 -0500

    add getProducts actions with filtering feature

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
+const productRouter = require('./routes/product');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(productRouter);
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => app.listen(process.env.PORT || 3000))
+  .catch(err => console.error(err));
--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,21 @@
 const Product = require('../models/product');
 
 exports.getProducts = async (req, res, next) => {
+  const searchQuery = makeSearchProductsQuery(req.query);
+
+  try {
+    const products = await searchQuery.exec();
+    let message = 'Products fetched.'
+
+    if (products.length === 0) message = 'No product found.';
+
+    res.status(200).json({
+      message,
+      products
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.createProduct = async (req, res, next) => {
@@ -15,6 +30,7 @@ exports.createProduct = async (req, res, next) => {
 
   try {
     const savedProduct = await product.save();
+
     res.status(201).json({
       message: 'Product successfully created.',
       savedProduct
@@ -25,5 +41,44 @@ exports.createProduct = async (req, res, next) => {
 };
 
 exports.getProduct = async (req, res, next) => {
+  const productId = req.params.productId;
 
+  try {
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      const error = new Error('Cannot find a plant with that ID.');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      message: 'Product fetched.',
+      product
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Private
+
+const makeSearchProductsQuery = queryParams => {
+  queryParams = castValuesToNum(queryParams);
+  return Product.find(queryParams);
+};
+
+const castValuesToNum = queryParams => {
+  const priceMin = queryParams.price_min || 0;
+  const priceMax = queryParams.price_max || 9999;
+  delete queryParams.price_min;
+  delete queryParams.price_max;
+
+  return {
+    ...queryParams,
+    price: {
+      $gte: priceMin,
+      $lt: priceMax
+    }
+  };
 };
