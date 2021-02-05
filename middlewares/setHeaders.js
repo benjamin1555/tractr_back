83eca108a624b917e74fe2870cce51918ffeeb40
Benjamin Teste <benjamin.teste@gmail.com>
2021-02-05 15:27:35 -0500

    modify setHeaders handler

--- middlewares/setHeaders.js
+++ middlewares/setHeaders.js
@@ -1,6 +1,7 @@
 const setHeader = (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 };
 
